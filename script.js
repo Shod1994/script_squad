414cb5cb1614000e44a85698df81e16b5fcb9768
Steven Silvestri <steven.c.silvestri@gmail.com>
2024-09-14 12:17:23 -0400

    JSON update

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 // fetch the cards to the gameBoard
 document.addEventListener("DOMContentLoaded", function () {
-  let cards = [
-    "images/monster1.png",
-    "images/monster2.png",
-    "images/monster3.png",
-    "images/monster4.png",
-    "images/monster5.png",
-    "images/monster6.png",
-    //Duplicate Images
-    "images/monster1.png",
-    "images/monster2.png",
-    "images/monster3.png",
-    "images/monster4.png",
-    "images/monster5.png",
-    "images/monster6.png",
+  // let cards = [
+  //   "images/monster1.png",
+  //   "images/monster2.png",
+  //   "images/monster3.png",
+  //   "images/monster4.png",
+  //   "images/monster5.png",
+  //   "images/monster6.png",
+  //   //Duplicate Images
+  //   "images/monster1.png",
+  //   "images/monster2.png",
+  //   "images/monster3.png",
+  //   "images/monster4.png",
+  //   "images/monster5.png",
+  //   "images/monster6.png",
+  // ];
+
+    let cards = [
+      {
+        "src": "images/monster1.png",
+        "value": 1
+      },
+      {
+        "src": "images/monster2.png",
+        "value": 2
+      },
+      {
+        "src": "images/monster3.png",
+        "value": 3
+      },
+      {
+        "src": "images/monster4.png",
+        "value": 4
+      },
+      {
+        "src": "images/monster5.png",
+        "value": 5
+      },
+      {
+        "src": "images/monster6.png",
+        "value": 6
+      }
   ];
 
   // Shuffle cards on each refresh/reset
