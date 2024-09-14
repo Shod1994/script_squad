65beb5ed08f8ad854a8f9d32ba80e8d1e4e7acdd
Lashod Jones <lashodj@gmail.com>
2024-09-14 00:08:43 -0400

    added flip functionality to cards ready for logic implementation

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 // fetch the cards to the gameBoard
+document.addEventListener('DOMContentLoaded', function() {
+    let cards = [
+        "images/monster1.png",
+        "images/monster2.png",
+        "images/monster3.png",
+        "images/monster4.png",
+        "images/monster5.png",
+        "images/monster6.png",
+        //Duplicate Images
+        "images/monster1.png",
+        "images/monster2.png",
+        "images/monster3.png",
+        "images/monster4.png",
+        "images/monster5.png",
+        "images/monster6.png",
+    ]
 
-// Shuffle cards
+    // Shuffle cards
+cards.sort(() => Math.random() - 0.5)
 
 // flip cards
+for(let i = 1; i <= 12; i++){
+    let selectCards = document.getElementById('image' + i);
+    selectCards.addEventListener('click', function() {
+    selectCards.src = cards[i - 1];
+    })
+}
 
 // Do the cards match
 
 // timer for the game
 
 //reset
+
+});
+
