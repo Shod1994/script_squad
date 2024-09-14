6fe071a21aef6feacae23dfecd59c3b7f5f2bf6c
Lashod Jones <lashodj@gmail.com>
2024-09-14 00:21:49 -0400

    updated comments in js for understanding

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // fetch the cards to the gameBoard
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let cards = [
         "images/monster1.png",
         "images/monster2.png",
@@ -16,22 +16,22 @@ document.addEventListener('DOMContentLoaded', function() {
         "images/monster6.png",
     ]
 
-    // Shuffle cards
-cards.sort(() => Math.random() - 0.5)
+    // Shuffle cards on each refresh/reset
+    cards.sort(() => Math.random() - 0.5)
 
-// flip cards
-for(let i = 1; i <= 12; i++){
-    let selectCards = document.getElementById('image' + i);
-    selectCards.addEventListener('click', function() {
-    selectCards.src = cards[i - 1];
-    })
-}
+    // flip cards
+    for (let i = 1; i <= 12; i++) {
+        let selectCards = document.getElementById('image' + i);
+        selectCards.addEventListener('click', function () {
+            selectCards.src = cards[i - 1]; //Makes sure 0 is included so the proper images can be fetched 
+        })
+    }
 
-// Do the cards match
+    // Do the cards match
 
-// timer for the game
+    // timer for the game
 
-//reset
+    //reset
 
 });
 
