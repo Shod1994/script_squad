659d6134c5c6717f631b9f8a7e20e017a00c2cd6
Lashod Jones <lashodj@gmail.com>
2024-09-14 10:52:34 -0400

    made changes

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 // fetch the cards to the gameBoard
-document.addEventListener('DOMContentLoaded', function () {
-    let cards = [
-        "images/monster1.png",
-        "images/monster2.png",
-        "images/monster3.png",
-        "images/monster4.png",
-        "images/monster5.png",
-        "images/monster6.png",
-        //Duplicate Images
-        "images/monster1.png",
-        "images/monster2.png",
-        "images/monster3.png",
-        "images/monster4.png",
-        "images/monster5.png",
-        "images/monster6.png",
-    ]
+document.addEventListener("DOMContentLoaded", function () {
+  let cards = [
+    "images/monster1.png",
+    "images/monster2.png",
+    "images/monster3.png",
+    "images/monster4.png",
+    "images/monster5.png",
+    "images/monster6.png",
+    //Duplicate Images
+    "images/monster1.png",
+    "images/monster2.png",
+    "images/monster3.png",
+    "images/monster4.png",
+    "images/monster5.png",
+    "images/monster6.png",
+  ];
 
-    // Shuffle cards on each refresh/reset
-    cards.sort(() => Math.random() - 0.5)
+  // Shuffle cards on each refresh/reset
+  cards.sort(() => Math.random() - 0.5);
 
-    // flip cards
-    for (let i = 1; i <= 12; i++) {
-        let selectCards = document.getElementById('image' + i);
-        selectCards.addEventListener('click', function () {
-            selectCards.src = cards[i - 1]; //Makes sure 0 is included so the proper images can be fetched 
-        })
-    }
+  // flip cards
+  for (let i = 1; i <= 12; i++) {
+    let selectCards = document.getElementById("image" + i);
+    selectCards.addEventListener("click", function () {
+      selectCards.src = cards[i - 1]; //Makes sure 0 is included so the proper images can be fetched
+    });
+  }
 
-    // Do the cards match
+  // Do the cards match
 
-    // timer for the game
+  // timer for the game
 
-    //reset
-
+  //reset
 });
-
